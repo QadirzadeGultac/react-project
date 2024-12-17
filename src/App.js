24007c8270e5b9aac52d0a8b951150042790a033
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-12-17 20:19:31 +0400

    deploy commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ function App() {
   const [imbd, setImbd] = useState([]);
   const [listMovieName, setListMovieName] = useState([]);
   return (
-      <Routes>
+      <div>
+        <Movie />
+        <Routes>
         <Route path='/' element={<Movie listName={listName} setListName={setListName} movieName={listMovieName} setMovieName={setListMovieName} imbdLink={imbd} setImbdLink={setImbd}/>} />
         <Route path='/favorite' element={<Favorite favorites={listName} movieNames={listMovieName} movieNameDelet={setListMovieName} imbdLinkDelete={setImbd} imbdlink={imbd}/>} />
       </Routes>
+      </div>
   );
 }
 
